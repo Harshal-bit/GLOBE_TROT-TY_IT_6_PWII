bf08e93eba8107ab54e3d9a5a5c6819a66b206c1
harshal-bit <harshaldeshpande30@gmail.com>
2023-02-17 01:25:01 +0530

    "Header Component and tailwing configured"

---
+++ globetrot/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   
+    // Or if using `src` directory:
+    "./src/components/*.js",
+    "./src/pages/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
