a6aaf41d5827a617d7ec7b19f80a43c00641300c
Harshal-bit <harshaldeshpande30@gmail.com>
2023-02-19 12:55:33 +0530

    modified scale of buttons

--- globetrot/src/components/Banner.js
+++ globetrot/src/components/Banner.js
@@ -12,8 +12,8 @@ function Banner() {
             />
             <div className="absolute  top-1/2 w-full text-center text-sm">
                 <p className="text-sm font-medium sm:text-base">Not sure where to stay? Perfect.</p>
-                <button className=" text-purple-500 bg-transparent px-5 py-2 rounded-full font-bold my-3 hover:shadow-2xl active:-scale-150 transition duration-150">
-                We are here to help
+                <button className=" text-purple-500 bg-transparent px-5 py-2 rounded-full font-bold my-3 hover:shadow-2xl active:scale-90 transition duration-150">
+                We are here to help.
                 </button>
             </div>
 
--- globetrot/src/components/Header.js
+++ globetrot/src/components/Header.js
@@ -39,7 +39,7 @@ function Header() {
       <div className="flex items-center space-x-4 justify-end text-gray-700">
         <p className="hidden md:inline-block cursor-pointer">Host Your Place</p>
         <HomeIcon className=" invisible md:visible h-6 cursor-pointer"/>
-        <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
+        <div className=" hover:shadow-lg  active:scale-90 transition duration-150 flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
           <ViewGridIcon className="h-6 "/>
           <UserCircleIcon className="h-6"/>
         </div>
