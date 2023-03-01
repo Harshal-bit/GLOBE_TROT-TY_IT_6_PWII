7f2cac9b523d1b2b469f6e54761f1f1624a8d87a
Bhatukumar Bairagi <bhatubairagi1610@gmail.com>
2023-03-01 16:54:17 +0530

    check

--- globetrot/src/components/Login/Login.js
+++ globetrot/src/components/Login/Login.js
@@ -72,7 +72,7 @@ function Login() {
                                 type="submit"
                                 className="group relative flex w-auto justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-2  text-xl font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-4"
                             >
-                                Verify Number
+                                Verify Numbers
                             </button>
                         </div>
                     </form>
