a5a7db4c710256b1b0934901ac47da451d3b9431
Harshal-bit <harshaldeshpande30@gmail.com>
2023-03-09 19:36:30 +0530

    Created Server and added api route to add/remove/update/findOne/findById single smallTile iitem

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 
-const app = express()
+const connectDB = require('./util/database');
+const smallTileController = require('./controllers/smallTile.controller');
 
-app.get('/', (req,res,next) => {
-    console.log(req.url)
-    console.log(req.method)
-    res.send("Server running")
+// import routes
+const smallTileRoutes = require('./routes/smallTile.route');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const options = {
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem')
+};
+
+const conn =  connectDB("mongodb+srv://harshald:harshald@cluster0.1zwtfi8.mongodb.net/globeTrot?retryWrites=true&w=majority");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    req.conn = conn;
+    next();
+});
+app.use('/smallTile',smallTileRoutes)
+
+// https.createServer(options,app).listen(4000,()=>{
+//     console.log("Server running on port 4000");
+// })
+
+app.listen(4000,options,()=>{
+    console.log("Server running on port 4000");
 })
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
-})
+
+
+
+
