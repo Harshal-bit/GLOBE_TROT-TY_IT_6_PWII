70e0635acf40508d95edcf489ba4c956494c5f81
Harshal-bit <harshaldeshpande30@gmail.com>
2023-05-01 23:32:00 +0530

    added listing creation

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -1,40 +1,38 @@
-import prisma from '@/libs/prismadb';
+import prisma from "@/libs/prismadb";
 
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-import getCurrentUser from '@/app/common/actions/getCurrentUser';
-export async function POST(
-    request:Request
-){
-    const currentUser = await getCurrentUser();
-    if(!currentUser){
-        return NextResponse.error();
-    }
-    const body = await request.json();
-    const {
-        title,
-        description,
-        imageSrc,
-        roomCount,
-        bathroomCount,
-        guestCount,
-        location,
-        price
-    } = body ;
+import getCurrentUser from "@/app/common/actions/getCurrentUser";
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const body = await request.json();
+  const {
+    title,
+    description,
+    imageSrc,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
 
-    const listing = await prisma.listing.create({
-        data: {
-            title,
-            description,
-            imageSrc,
-            roomCount,
-            bathroomCount,
-            guestCount,
-            locationValue: location.value,
-            price:parseInt(price,10),
-            userId:currentUser.id
-        }
-    });
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
 
-    return NextResponse.json(listing);
+  return NextResponse.json(listing);
 }
