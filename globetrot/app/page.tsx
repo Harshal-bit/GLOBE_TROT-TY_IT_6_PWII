70ce1d877bd4dbd7980e800701d34217fb02c262
Bhatukumar Bairagi <bhatubairagi1610@gmail.com>
2023-05-04 00:04:54 +0530

    Add card component

--- globetrot/app/page.tsx
+++ globetrot/app/page.tsx
@@ -1,38 +1,56 @@
 import Container from "@/app/common/components/layout/Container";
 
-import EmptyState from "@/app/common/components/layout/EmptyState";
 
 import getListings, { IListingsParams } from "@/app/common/actions/getListings";
 import getCurrentUser from "@/app/common/actions/getCurrentUser";
+import ClientOnly from "./common/components/ClientOnly";
+
+import EmptyState from "./common/components/EmptyState";
+import ListingCard from "./common/components/listings/ListingCard";
 
 interface HomeProps {
   searchParams: IListingsParams
 };
 
 const Home = async ({ searchParams }: HomeProps) => {
-  
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
   
 
   return (
-    <Container>
-      <div 
-        className="
-          pt-52
-          pb-20
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
-      >
-      </div>
-    </Container>
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
 
